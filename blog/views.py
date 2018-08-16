94166a237806167f9dc98eda41935b1e89a2ad49
Your Name <t_j4331@naver.com>
2018-08-16 09:39:52 +0900

    홈페이지 레이아웃 수정

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,22 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
+from django.shortcuts import render
 
-from .models import Post
-from .forms import PostForm
+def profile(request):
+    return render(request, 'blog/profile.html', {})
 
+def game(request):
+    return render(request, 'blog/game.html', {})
 
-# Create your views here.
-from blog.models import Post
+def calculator(request):
+    return render(request, 'blog/calculator.html', {})
 
-
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def image_viewer(request):
+    return render(request, 'blog/image_viewer.html', {})
 
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = User.objects.get(username='admin')
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('blog:post_list')
+def contact(request):
+    return render(request, 'blog/contact.html', {})
 
-    else:
-        form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
