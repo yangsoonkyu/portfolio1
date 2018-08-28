9158154332014f58c437e11923b95f2f3e69448b
Your Name <t_j4331@naver.com>
2018-08-28 15:48:35 +0900

    목록 및 레아아웃 수정, 이미지뷰어 추가

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.profile, name='profile'),
-    url(r'^game$', views.game, name='game'),
-    url(r'^calculator$', views.calculator, name='calculator'),
-    url(r'^image_viewer$', views.image_viewer, name='image_viewer'),
+    url(r'^resume$', views.resume, name='resume'),
+    url(r'^portfolio$', views.portfolio, name='portfolio'),
+    url(r'^portfolio/calculator$', views.calculator, name='calculator'),
+    url(r'^portfolio/game$', views.game, name='game'),
+    url(r'^portfolio/image_viewer$', views.image_viewer, name='image'),
     url(r'^contact$', views.contact, name='contact'),
+    url(r'^design$', views.design, name='design'),
 
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 
 def profile(request):
-    return render(request, 'blog/profile.html', {})
+    return render(request, 'blog/profile.html')
 
-def game(request):
-    return render(request, 'blog/game.html', {})
+def resume(request):
+    return render(request, 'blog/resume.html', {})
+
+def portfolio(request):
+    return render(request, 'blog/portfolio.html', {})
 
 def calculator(request):
-    return render(request, 'blog/calculator.html', {})
+    return render(request, 'blog/calculator_site.html', {})
+
+def game(request):
+    return render(request, 'blog/game_site.html', {})
 
 def image_viewer(request):
-    return render(request, 'blog/image_viewer.html', {})
+    return render(request, 'blog/image_viewer_site.html', {})
 
 
 def contact(request):
     return render(request, 'blog/contact.html', {})
+
+def design(request):
+    return render(request, 'blog/design.html', {})
 
 
 
