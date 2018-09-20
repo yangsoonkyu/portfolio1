363a22d775c561608532c2dcb35b98fd9ec6cf67
Your Name <t_j4331@naver.com>
2018-09-20 17:19:52 +0900

    포스트 수정, 삭제, 배포기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -12,8 +12,11 @@ urlpatterns = [
     url(r'^contact$', views.contact, name='contact'),
     url(r'^design$', views.design, name='design'),
     url(r'^board$', views.PostListView.as_view(), name='board'),
+    url(r'^save_board$', views.savePostListView.as_view(), name='save_board'),
     url('^post/(?P<pk>[0-9]+)/$', views.postDetailView.as_view(), name='post_detail'),
     url(r'^post/new$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from django.views import generic
 
 from blog.forms import PostForm
@@ -41,12 +42,28 @@ class postDetailView(generic.DetailView):
 
 
 
+
 class PostListView(generic.ListView):
     model = Post
     template_name = 'blog/board.html'
 
     context_object_name = 'posts'
+    queryset = Post.objects.filter(published_date__lte=timezone.now())
     paginate_by = 7
+
+class savePostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/save_board.html'
+
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    paginate_by = 7
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog:post_detail', pk=pk)
 
 
 def post_new(request):
@@ -79,6 +96,11 @@ def post_edit(request, pk):
                 post.save()
                 return redirect('blog:post_detail', pk=post.pk)
         else:
-            test=1
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog:board')
